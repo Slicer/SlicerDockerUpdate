401d290606f33dc94c266e7b681aeaf10fb8ee97
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-04-15 00:37:07 -0400

    setup: Checkout Slicer sources as git only instead of git-svn

--- setup.sh
+++ setup.sh
@@ -16,26 +16,13 @@ slicerdocker_dir=$script_dir/SlicerDocker
 #
 if [[ ! -d $slicer_dir ]];  then
   git clone git://github.com/Slicer/Slicer $slicer_dir
-
-  pushd $slicer_dir > /dev/null
-
-  # Instructions from https://www.slicer.org/wiki/Documentation/Nightly/Developers/Build_Instructions#CHECKOUT_slicer_source_files
-  git svn init http://svn.slicer.org/Slicer4/trunk
-  git update-ref refs/remotes/git-svn refs/remotes/origin/master
-  git checkout master
-  git svn rebase
-
-  git config --global svn.rmdir true
-
-  popd > /dev/null
-
 fi
 
 #
 # Checkout SlicerDocker sources
 #
 if [[ ! -d $slicerdocker_dir ]]; then
-  git clone git://github.com/thewtex/SlicerDocker $slicerdocker_dir
+  git clone git://github.com/Slicer/SlicerDocker $slicerdocker_dir
 fi
 
 #
